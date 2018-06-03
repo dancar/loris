f76a1429823459d0c383602e48585aa44b3ced83
Dan <dan@carmon.org.il>
2018-06-03 22:02:53 +0200

    Initial, show stadiums

---
+++ loris.rb
@@ -0,0 +1,12 @@
+require 'byebug'
+require 'sinatra'
+require 'httparty'
+
+URL = 'https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json'
+
+
+get '/' do
+  response = HTTParty.get(URL)
+  data = JSON.parse(response)
+  erb :index, locals: {data: data}
+end
